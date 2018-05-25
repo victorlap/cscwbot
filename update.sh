caef39d5350039ef9e1401fab5e6eb28bb606554
Victor Lap <victorlap@outlook.com>
2018-05-25 14:17:42 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -7,8 +7,8 @@ composer install
 php7 artisan migrate --force
 
 php7 artisan cache:clear
+php7 artisan view:clear
 php7 artisan config:cache
-php7 artisan view:cache
 php7 artisan route:cache
 
 php7 artisan up
