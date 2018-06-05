d7b7a55688f9bfae6c89e7b35798760b30ab08db
Martijn Gemmink <m.gemmink@bullit.solutions>
2018-06-05 22:04:56 +0200

    List viewpoints

---
+++ app/Http/Controllers/ListViewpointsController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Clients\Slack;
+use App\Viewpoint;
+use BotMan\BotMan\BotMan;
+use BotMan\BotMan\Exceptions\Base\BotManException;
+use BotMan\BotMan\Interfaces\UserInterface;
+use Illuminate\Support\Facades\DB;
+use GuzzleHttp\Exception\RequestException;
+use Illuminate\Support\Facades\Log;
+use Psr\Http\Message\ResponseInterface;
+
+class ListViewpointsController extends Controller
+{
+    protected $botman;
+
+    protected $bot;
+
+    protected $user;
+
+    protected $name;
+
+    protected $channel;
+
+    protected $discussion;
+
+    /**
+     * @param BotMan $bot
+     * @param string $name
+     */
+    public function __invoke($bot)
+    {
+        $this->botman = $bot;
+        $this->user = $bot->getUser();
+
+        $bot->reply("Got it. Collecting the viewpoints now.");
+        $this->listViewpoints();
+
+    }
+
+    public function listViewpoints()
+    {
+
+        $discussion = DB::table('discussions')->where('discussion_channel', $this->botman->getMessage()->getRecipient());
+        $viewpoints = DB::table('viewpoints')->where('discussion_id', $discussion->id);
+
+        Log::debug('Number of viewpoints =  ' . count($viewpoints));
+
+        $this->discussion = $discussion->id;
+
+        $this->bot->reply(sprintf(
+            "There are %s viewpoint(s) for this discussion. The first one is %s",
+            $this->count($viewpoints),
+            $this->$viewpoints[0]->viewpoint
+        ));
+
+    }
+}
--- routes/botman.php
+++ routes/botman.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AddArgumentController;
 use App\Http\Controllers\HelpController;
+use App\Http\Controllers\ListViewpointsController;
 use App\Http\Controllers\AddViewpointController;
 use App\Http\Controllers\StartDiscussionController;
 
@@ -11,5 +12,6 @@ $botman->hears('/discussion help', HelpController::class);
 $botman->hears('/discussion {name}', StartDiscussionController::class);
 
 $botman->hears('/viewpoint {name}', AddViewpointController::class);
+$botman->hears('/viewpoints', ListViewpointsController::class);
 
 $botman->hears('/argument {name}', AddArgumentController::class);
