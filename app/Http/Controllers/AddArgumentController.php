e89c8fada61d96261da982c0cd29cdbb93d9ca4f
Martijn Gemmink <m.gemmink@bullit.solutions>
2018-06-05 20:06:02 +0200

    Add argument first attempt

--- app/Http/Controllers/AddArgumentController.php
+++ app/Http/Controllers/AddArgumentController.php
@@ -2,16 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Clients\Slack;
+use App\Argument;
 use BotMan\BotMan\BotMan;
+use BotMan\BotMan\Exceptions\Base\BotManException;
+use BotMan\BotMan\Interfaces\UserInterface;
+use GuzzleHttp\Exception\RequestException;
+use Illuminate\Support\Facades\Log;
+use Psr\Http\Message\ResponseInterface;
 
 class AddArgumentController extends Controller
 {
+
+    protected $botman;
+
+    protected $bot;
+
+    protected $user;
+
+    protected $name;
+
+    protected $channel;
+
     /**
      * @param BotMan $bot
      * @param string $name
      */
     public function __invoke($bot, $name)
     {
-        // TODO: Implement __invoke() method.
+        $this->botman = $bot;
+        $this->name = $name;
+        $this->user = $bot->getUser();
+
+        $bot->reply("Hello, your argument was added");
+
     }
 }
