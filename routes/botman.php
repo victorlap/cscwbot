46b504311c5b307ebd07eb5f5885ed3b39fc1449
Martijn Gemmink <m.gemmink@bullit.solutions>
2018-06-05 20:22:42 +0200

    Added routing

--- routes/botman.php
+++ routes/botman.php
@@ -2,11 +2,14 @@
 
 use App\Http\Controllers\AddArgumentController;
 use App\Http\Controllers\HelpController;
+use App\Http\Controllers\AddViewpointController;
 use App\Http\Controllers\StartDiscussionController;
 
 $botman = resolve('botman');
 
 $botman->hears('/discussion help', HelpController::class);
 $botman->hears('/discussion {name}', StartDiscussionController::class);
+
+$botman->hears('/viewpoint {name}', AddViewpointController::class);
 
 $botman->hears('/argument {name}', AddArgumentController::class);
