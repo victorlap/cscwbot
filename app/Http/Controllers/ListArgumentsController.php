3dc0dd993caa9e203827b89274739b873e31a695
Martijn Gemmink <m.gemmink@bullit.solutions>
2018-06-08 17:04:00 +0200

    List arguments by viewpoint first attempt

--- app/Http/Controllers/ListArgumentsController.php
+++ app/Http/Controllers/ListArgumentsController.php
@@ -2,9 +2,60 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Discussion;
+use BotMan\BotMan\BotMan;
 
 class ListArgumentsController extends Controller
 {
+    protected $botman;
 
+    protected $bot;
+
+    protected $user;
+
+    protected $name;
+
+    protected $channel;
+
+    /**
+     * @param BotMan $bot
+     */
+    public function __invoke($bot)
+    {
+        $this->botman = $bot;
+        $this->user = $bot->getUser();
+
+        $this->botman->reply($this->listViewpoints($this->botman->getMessage()->getRecipient()));
+
+    }
+
+    public static function listArguments($channel)
+    {
+
+        $discussion = Discussion::where('discussion_channel', $channel)->first();
+        $viewpoints = $discussion->viewpoints;
+        $viewpoints_string = '';
+        foreach ($viewpoints as $viewpoint) {
+            $arguments_string = '';
+            foreach ($viewpoint->arguments as $argument) {
+                $arguments_string .= sprintf(
+                    "\n%s",
+                    $argument->argument
+                );
+            }
+            $viewpoints_string .= sprintf(
+                "\n*%s*, supporting arguments:",
+                $viewpoint->viewpoint,
+                $arguments_string
+            );
+        }
+
+        return (
+        sprintf(
+            "The viewpoints and their arguments are: %s",
+            $viewpoints_string
+        )
+        );
+
+    }
 }
--- routes/botman.php
+++ routes/botman.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AddArgumentController;
+use App\Http\Controllers\ListArgumentsController;
 use App\Http\Controllers\AddViewpointController;
 use App\Http\Controllers\GotoRoundController;
 use App\Http\Controllers\HelpController;
@@ -15,6 +16,7 @@ $botman->hears('/discussion {name}', StartDiscussionController::class);
 $botman->hears('/viewpoint list', ListViewpointsController::class);
 $botman->hears('/viewpoint {name}', AddViewpointController::class);
 
+$botman->hears('/argument list', ListArgumentsController::class);
 $botman->hears('/argument {argument}', AddArgumentController::class);
 
 $botman->hears('/goto {round}', GotoRoundController::class);
