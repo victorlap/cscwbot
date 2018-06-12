6970ce726d17b5f0d62dc379cb30b972ee1881a6
Victor Lap <victorlap@outlook.com>
2018-06-12 19:26:40 +0200

    Log

--- app/Http/Controllers/EndDiscussionController.php
+++ app/Http/Controllers/EndDiscussionController.php
@@ -31,9 +31,9 @@ class EndDiscussionController extends Controller
     {
         $this->botman = $bot;
         $this->viewpoint = $viewpoint;
-        Log::info('viewpoint', [$viewpoint, $this->discussion->viewpoints()->pluck('id')]);
         $this->user = $bot->getUser();
         $this->discussion = Discussion::where('discussion_channel', $this->botman->getMessage()->getRecipient())->first();
+        Log::info('viewpoint', [$viewpoint, $this->discussion->viewpoints()->pluck('id')]);
 
         if(! $this->discussion->viewpoints()->pluck('id')->has($viewpoint)) {
             $this->botman->reply("Invalid viewpoint id, try listing viewpoints with /viewpoint list");
